f70fd588960bc4eab4cd9f7099b580ace794439b
grcoan <grcoan@umich.edu>
2018-05-03 01:34:43 -0400

    added scores page and working steppers to increase scores. Need to figure out how to make the app remember these values after the app is quit

--- chel-tracker/ViewController.swift
+++ chel-tracker/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //score labels
+    @IBOutlet weak var scoreZhang: UILabel!
+    @IBOutlet weak var scoreAnderson: UILabel!
+    
+    //score steppers
+    @IBAction func stepperZhang(_ sender: UIStepper){
+        scoreZhang.text = String(sender.value)
+    }
+    @IBAction func stepperAnderson(_ sender: UIStepper){
+        scoreAnderson.text = String(sender.value)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
