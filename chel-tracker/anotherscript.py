b7db6bc51b2955a01e0a47325ce648a326fcec4e
Christopher Zhang <christopher.zhang@clinc.com>
2018-11-13 22:58:46 -0500

    more function

--- chel-tracker/anotherscript.py
+++ chel-tracker/anotherscript.py
@@ -1,5 +1,14 @@
 import datetime
 import json
 
-def stupid():
+def stupid(newParameter):
 	print("BIG DICK")
+	print("another")
+
+def smorestupid(dumb, dumber):
+	print("8====>")
+
+def evenMoreStupid():
+	i = 0
+	j = 1
+	k = 69
